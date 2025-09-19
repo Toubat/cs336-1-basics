a4f736e5cd8934e2b477078b95ac690b8ca04739
Toubat <toubatbrian@gmail.com>
2025-09-19 17:17:17 +0800

    add

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from cs336_basics.bpe.train import run_train_bpe
 from cs336_basics.bpe.utils import bytes_to_unicode
@@ -6,16 +7,22 @@ from cs336_basics.bpe.utils import bytes_to_unicode
 
 def main():
     vocab, merges = run_train_bpe(
-        input_path="data/TinyStoriesV2-GPT4-train.txt",
+        input_path="data/owt_train.txt",
         vocab_size=10000,
         special_tokens=["<|endoftext|>"],
         verbose=True,
     )
 
-    with open("test_vocab.json", "w") as f:
-        json.dump({v: str(b) for v, b in vocab.items()}, f, indent=2)
+    os.makedirs("outputs", exist_ok=True)
 
-    with open("test_merges.txt", "w") as f:
+    with open("outputs/owt_vocab.json", "w") as f:
+        json.dump(
+            {v: bytes_to_unicode(token_bytes) for v, token_bytes in vocab.items()},
+            f,
+            indent=2,
+        )
+
+    with open("outputs/owt_merges.txt", "w") as f:
         for merge in merges:
             f.write(f"{bytes_to_unicode(merge[0])} {bytes_to_unicode(merge[1])}\n")
 
