cbb1be54558fe2604d7ba00adae1187818effc45
Toubat <toubatbrian@gmail.com>
2025-11-09 23:55:57 +0800

    add

---
+++ cs336_basics/train.py
@@ -0,0 +1,25 @@
+import random
+
+import wandb
+
+run = wandb.init(
+    entity="yoasobyin-n-a",
+    project="cs336",
+    config={
+        "learning_rate": 0.02,
+        "architecture": "CNN",
+        "dataset": "CIFAR-100",
+        "epochs": 10,
+    },
+)
+
+# Simulate training.
+epochs = 10
+offset = random.random() / 5
+for epoch in range(2, epochs):
+    acc = 1 - 2**-epoch - random.random() / epoch - offset
+    loss = 2**-epoch + random.random() / epoch + offset
+
+    run.log({"acc": acc, "loss": loss})
+
+run.finish()
